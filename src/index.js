75095ee6a3f5d4b5dc3e460a468df0dc9a07a953
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-12-04 11:51:01 +0900

    add grid layout and early function logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+import { format, compareAsc } from "date-fns";
+
+
+function createTodo(Todo, description, date, priority) {
+    const title = Todo;
+    const info = description;
+    const dueDate = date;
+    const importance = priority;
+
+    return { title, info, dueDate, importance };
+}
+
+const dogwalk = createTodo('Walk the dog', "Take sally", Date.now(), '3');
+console.log(dogwalk);
+
+
+function createProject() {
+    // will be called when new project button is clicked.
+    //create blank project list 
+}
+
+function displayProject() {
+    //will be called when new project button is clicked.
+    // will add the created blank project to the DOM
+}
