229693ca23b602692fe9ca2c51f87eb2c2e36e8f
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-12-05 12:38:47 +0900

    create modal to get user input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { format, compareAsc } from "date-fns";
+import { formatRelative, sub, subDays } from "date-fns";
 
 
 function createTodo(Todo, description, date, priority) {
@@ -7,12 +7,32 @@ function createTodo(Todo, description, date, priority) {
     const info = description;
     const dueDate = date;
     const importance = priority;
+    const testDate = formatRelative( subDays(new Date(), 1), new Date());
 
-    return { title, info, dueDate, importance };
+
+    return { title, info, testDate, importance };
 }
 
-const dogwalk = createTodo('Walk the dog', "Take sally", Date.now(), '3');
+const dogwalk = createTodo('Walk the dog', "Take sally", Date(), '3');
 console.log(dogwalk);
+
+
+const todoDialog = document.querySelector("#newTodo");
+const newTodoBtn = document.querySelector(".addTodo");
+
+const todoTitle = document.querySelector("#todo-title");
+const todoDescription = document.querySelector("#todo-description");
+const todoDueDate = document.querySelector("#todo-dueDate");
+const todoPriority = document.querySelector("#todo-priority");
+
+newTodoBtn.addEventListener("click", () => {
+    todoDialog.showModal();
+})
+const confirmBtn = document.querySelector("#confirmBtn");
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value);
+})
 
 
 function createProject() {
