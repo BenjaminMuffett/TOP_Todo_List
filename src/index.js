c0b46703699fc61768c27c895dd07d131623ea03
Benjamin Muffett <136661576+BenjaminMuffett@users.noreply.github.com>
2024-12-06 10:02:31 +0900

    clear modal input after cancel or add todo

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
 import "./styles.css";
-import { formatRelative, sub, subDays } from "date-fns";
+import { formatRelative, sub, subDays, endOfDay, endOfToday } from "date-fns";
 
 
-function createTodo(Todo, description, date, priority) {
-    const title = Todo;
-    const info = description;
-    const dueDate = date;
-    const importance = priority;
-    const testDate = formatRelative( subDays(new Date(), 1), new Date());
+function createTodoObject() {
+    const todoTitle = document.querySelector("#todo-title");
+    const todoDescription = document.querySelector("#todo-description");
+    const todoDueDate = document.querySelector("#todo-dueDate");
+    const todoPriority = document.querySelector("#todo-priority");
 
+    const title = todoTitle.value;
+    const info = todoDescription.value;
+    const dueDate = todoDueDate.value;
+    const importance = todoPriority.value;
 
-    return { title, info, testDate, importance };
+    return { title, info, dueDate, importance };
 }
 
-const dogwalk = createTodo('Walk the dog', "Take sally", Date(), '3');
-console.log(dogwalk);
-
+// function clearDialog(modal) {
+//     // const todoTitle = document.querySelector("#todo-title");
+//     // const todoDescription = document.querySelector("#todo-description");
+//     // const todoDueDate = document.querySelector("#todo-dueDate");
+//     // const todoPriority = document.querySelector("#todo-priority");
+//     modal.title = '';
+//     modal.info = '';
+//     modal.dueDate = '';
+//     modal.importance = '';
+// }
 
 const todoDialog = document.querySelector("#newTodo");
 const newTodoBtn = document.querySelector(".addTodo");
 
-const todoTitle = document.querySelector("#todo-title");
-const todoDescription = document.querySelector("#todo-description");
-const todoDueDate = document.querySelector("#todo-dueDate");
-const todoPriority = document.querySelector("#todo-priority");
-
 newTodoBtn.addEventListener("click", () => {
     todoDialog.showModal();
-})
+    const todoForm = document.querySelector("#todo-form");
+    todoForm.reset();
+});
+
+const cancelBtn = document.querySelector(".cancelBtn");
+cancelBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    todoDialog.close();
+});
+
 const confirmBtn = document.querySelector("#confirmBtn");
 confirmBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value);
-})
+    let testCat = createTodoObject()
+    console.log(testCat);
+    // let v = createTodoObject(todoTitle.value, todoDescription.value, todoDueDate.value, todoPriority.value);
+    // console.log(v);
+    todoDialog.close();
+});
+
+
 
 
 function createProject() {
